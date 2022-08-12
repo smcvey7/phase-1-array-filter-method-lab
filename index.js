9dc30c13662cdf35eb984ed66a93a7616a23a0b6
Steven McVey <smcvey1210@gmail.com>
2022-08-11 17:22:32 -0700

    commit tree cee695593d4d3c5d824986af2cd5893017360d8d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(array, driver){
+    return array.filter(item => item.toLowerCase() === driver.toLowerCase())
+  }
+
+function fuzzyMatch(array, search){
+  return array.filter(driver =>{
+    let outcome=false;
+    for (let i = 0; i < search.length; i++){
+      if (search[i].toLowerCase() === driver[i].toLowerCase()){
+        outcome = true;
+      } else{
+        outcome = false;
+        return outcome;
+      }
+    }
+    return outcome;
+  })
+}
+
+function matchName(array, search){
+    return array.filter(item => item.name === search)
+  }
